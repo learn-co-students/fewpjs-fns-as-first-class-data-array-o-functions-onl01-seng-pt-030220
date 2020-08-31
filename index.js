51b58656c9f0c305972e07e198c3c6cfb7038dee
github username <github email address>
2020-08-31 15:20:43 +0100

    Done.

---
+++ index.js
@@ -0,0 +1,55 @@
+function wakeDog(dogName, dogBreed){
+const message = `Wake ${dogName} the ${dogBreed}`
+console.log(message)
+return message
+}
+
+function leashDog(dogName, dogBreed){
+    const message = `Leash ${dogName} the ${dogBreed}`
+    console.log(message)
+    return message
+    }
+
+    function walkToPark(dogName, dogBreed){
+        const message = `Walk to the park with ${dogName} the ${dogBreed}`
+        console.log(message)
+        return message
+        }
+
+        function throwFrisbee(dogName, dogBreed){
+            const message = `Throw the frisbee for ${dogName} the ${dogBreed}`
+            console.log(message)
+            return message
+            }
+
+            function walkHome(dogName, dogBreed){
+                const message = `Walk home with ${dogName} the ${dogBreed}`
+                console.log(message)
+                return message
+                }
+
+                function unleashDog(dogName, dogBreed){
+                    const message = `Unleash ${dogName} the ${dogBreed}`
+                    console.log(message)
+                    return message
+                    }
+
+
+const routine = [
+    wakeDog,
+    leashDog,
+    walkToPark,
+    throwFrisbee,
+    walkHome,
+    unleashDog]
+
+const returnedArray = []
+
+function exerciseDog(dogName, dogBreed){
+    routine.forEach(action => {
+    var meow = action(dogName,dogBreed)
+    returnedArray.push(meow)
+    } 
+)
+return returnedArray
+}
